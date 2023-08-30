91ae215e1d2fced3b2fb14b4bc16f0f272109c7d
Remco Stoeten <remcostoeten@hotmail.com>
2023-08-30 19:14:53 +0200

    Add menu in tsx.

---
+++ config/navigation-routes.ts
@@ -0,0 +1,46 @@
+export const navigationRoutes = {
+  mainNav: [
+    {
+      title: "Dashboard",
+      href: "/dashboard",
+    },
+    {
+      title: "Python script generator",
+      href: "/python",
+    },
+    {
+      title: "Playground showcase",
+      href: "/playground",
+    },
+    {
+      title: "HTML to JSX",
+      href: "/html-to-jsx",
+    },
+    {
+      title: "Geolocation",
+      href: "/geolocation",
+    },
+    {
+      title: "Url extractor",
+      href: "https://vsc.remcostoeten.com/url",
+      target: "_blank",
+    },
+    {
+      title: "Todos",
+      href: "/todo"
+    }, 
+    { 
+      title: "Expenses",
+      href: "/income"
+    },
+    {
+      title: "Thoughts",
+      href: "/thoughts",
+    },
+    {    
+      title: "Docs",
+       label: "WiP",
+      href: "/docs/docs",
+    }
+  ],
+}
