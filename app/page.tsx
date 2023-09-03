8d22d003594c3a3337acece2ab53c4639d4521c4
Remco Stoeten <remcostoeten@hotmail.com>
2023-09-03 13:30:48 +0200

    work on home

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import SectionDivider from "@/components/divider";
+import About from "./python/components/home/about";
 import Intro from "./python/components/home/intro";
 
 export default function Home() {
   return (
     <>
         <Intro />
+        <SectionDivider/>
+        <About/>
     </>
   );
 }
