b59a93ad7c797f66311613fc0aa14fbde9e23270
Remco Stoeten <remcostoeten@hotmail.com>
2023-09-03 13:30:31 +0200

    Modify intro

---
+++ app/python/components/home/about.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "@c/section-heading";
+import { motion } from "framer-motion";
+import Link from 'next/link';
+export default function About() {
+  return (
+    <motion.section
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+        I am Remco Stoeten, 26, from Lemmer, Netherlands. I hold a graphic
+        design degree and have over five years of experience in frontend
+        development, specifically in custom 🤮Magento 2🤢 webshops.</p><p className="mb-3"> After working at{" "}
+        <Link className="underline" href="https://tickles.nl" target="_blank" rel="noopener noreferrer">an agency</Link> and then in-house at a multinational SaaS company, I joined
+        {" "} <Link className="underline" href="https://gitlab.com/pleio" target="_blank" rel="noopener noreferrer">Pleio</Link>, which exclusively operates in non-profit, contributed to projects
+        like{" "}      </p><p className="mb-3">
+        <Link className="underline"
+          href="https://gitlab.com/pleio/dossier/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          FSV
+        </Link>
+        . I have a strong passion for complex interfaces and all-round
+        programming, always looking to learn new things and improve my skills. Currently focusing on Typescript & co. Also some Python and Lua on the side.
+      </p>
+    </motion.section>
+  );
+}
--- app/python/components/home/intro.tsx
+++ app/python/components/home/intro.tsx
@@ -1,107 +1,128 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import React from 'react';
-import { motion } from 'framer-motion';
-import Link from 'next/link';
-import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
-import { HiDownload } from 'react-icons/hi';
-import { FaGithubSquare } from 'react-icons/fa';
-import { useSectionInView } from '@/hooks/useSectionInview';
-import { useActiveSectionContext } from '@/context/active-section-contex';
-import AnimatedTextWord from '../../../../components/AnimateTextWord';
+import Image from "next/image";
+import React from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
+import { useSectionInView } from "@/hooks/useSectionInview";
+import { useActiveSectionContext } from "@/context/active-section-contex";
+import AnimatedTextWord from "../../../../components/AnimateTextWord";
+import AnimatedTextCharacter from "../../../../components/AnimateTextWord";
 
 export default function Intro() {
-    const { ref } = useSectionInView('Home', 0.5);
-    const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
+  const { ref } = useSectionInView("Home", 0.5);
+  const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
 
-    return (
-        <section ref={ref} id="home" className="mb-28 mx-auto max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
-            <div className="flex items-center justify-center">
-                <div className="relative">
-                    <motion.div
-                        initial={{ opacity: 0, scale: 0 }}
-                        animate={{ opacity: 1, scale: 1 }}
-                        transition={{
-                            type: 'tween',
-                            duration: 0.2,
-                        }}>
-                        <Image
-                            src="https://media.licdn.com/dms/image/C5603AQFIlU2oV2JEgg/profile-displayphoto-shrink_800_800/0/1638502874210?e=2147483647&v=beta&t=NQAizH_x-gBtMhmn1APmsKcl9QjmasnaZ60TqG-ZhR4"
-                            alt="Remco Stoeten"
-                            width="192"
-                            height="192"
-                            quality="95"
-                            priority={true}
-                            className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
-                        />
-                    </motion.div>
+  return (
+    <section
+      ref={ref}
+      id="home"
+      className="mb-28 mx-auto max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+    >
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "tween",
+              duration: 0.2,
+            }}
+          >
+            <Image
+              src="https://media.licdn.com/dms/image/C5603AQFIlU2oV2JEgg/profile-displayphoto-shrink_800_800/0/1638502874210?e=2147483647&v=beta&t=NQAizH_x-gBtMhmn1APmsKcl9QjmasnaZ60TqG-ZhR4"
+              alt="Remco Stoeten"
+              width="192"
+              height="192"
+              quality="95"
+              priority={true}
+              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+            />
+          </motion.div>
 
-                    <motion.span
-                        className="absolute bottom-0 right-0 text-4xl"
-                        initial={{ opacity: 0, scale: 0 }}
-                        animate={{ opacity: 1, scale: 1 }}
-                        transition={{
-                            type: 'spring',
-                            stiffness: 125,
-                            delay: 0.1,
-                            duration: 0.7,
-                        }}>
-                        <span className="wave">👋</span>
-                    </motion.span>
-                </div>
-            </div>
+          <motion.span
+            className="absolute bottom-0 right-0 text-4xl"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            <span className="wave">👋</span>
+          </motion.span>
+        </div>
+      </div>
 
-            <motion.h1 className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl" initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }}>
-                <span className="font-bold">
-                    <AnimatedTextWord text=" Hello, I'm Remco." direction={undefined} />
-                </span>{' '}
-                I'm a <span className="font-bold">front-end developer</span> with <span className="font-bold">~7 years</span> of experience. I enjoy building things,{' '}
-                <span className="italic">with scode</span>. My current focus is <span className="underline">Next.js</span>.
-            </motion.h1>
+      <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <AnimatedTextCharacter>
+          <span className="font-bold">I'm Remco.</span>
+        </AnimatedTextCharacter>
+        <AnimatedTextCharacter>
+          I'm a <span className="font-bold">front-end developer</span> with{" "}
+          <span className="font-bold">~7 years</span> of experience. I enjoy
+          building things, <span className="italic">with code</span>.
+        </AnimatedTextCharacter>
+      </motion.h1>
 
-            <motion.div
-                className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
-                initial={{ opacity: 0, y: 100 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{
-                    delay: 0.1,
-                }}>
-                <Link
-                    href="#contact"
-                    className="group bg-black text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
-                    data-cursor-hover
-                    onClick={() => {
-                        setActiveSection('Contact');
-                        setTimeOfLastClick(Date.now());
-                    }}>
-                    Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
-                </Link>
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-black text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          data-cursor-hover
+          onClick={() => {
+            setActiveSection("Contact");
+            setTimeOfLastClick(Date.now());
+          }}
+        >
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
 
-                <Link
-                    data-cursor-hover
-                    className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack black border"
-                    href="/CV.pdf"
-                    download>
-                    Download CV <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
-                </Link>
+        <Link
+          data-cursor-hover
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border  dark:bg-black "
+          href="/CV.pdf"
+          download
+        >
+          Download CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </Link>
 
-                <Link
-                    data-cursor-hover
-                    className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack black border dark:text-white/60"
-                    href="https://linkedin.com"
-                    target="_blank">
-                    <BsLinkedin />
-                </Link>
+        <Link
+          data-cursor-hover
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-black border dark:text-white/60"
+          href="https://www.linkedin.com/in/remco-stoeten/"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </Link>
 
-                <Link
-                    data-cursor-hover
-                    className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer black border dark:text-white/60"
-                    href="https://github.com"
-                    target="_blank">
-                    <FaGithubSquare />
-                </Link>
-            </motion.div>
-        </section>
-    );
+        <Link
+          data-cursor-hover
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer dark:bg-black border dark:text-white/60"
+          href="https://github.com/remcostoeten"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </Link>
+      </motion.div>
+    </section>
+  );
 }
