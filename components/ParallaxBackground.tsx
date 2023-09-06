891728ebfbe15fe6ffc922bb9986bac8f3b691dd
Remco Stoeten <remcostoeten@hotmail.com>
2023-09-06 10:10:30 +0200

    Building.

--- components/ParallaxBackground.tsx
+++ components/ParallaxBackground.tsx
@@ -5,7 +5,6 @@ import { motion, useAnimation } from 'framer-motion';
 const ParallaxDiv: React.FC = () => {
   const controls = useAnimation();
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-  const [delayed, setDelayed] = useState(false); // State to control the delay
 
   const handleMouseMove = (e) => {
     setMousePosition({ x: e.clientX, y: e.clientY });
@@ -20,34 +19,22 @@ const ParallaxDiv: React.FC = () => {
     window.addEventListener('scroll', updateScroll);
     window.addEventListener('mousemove', handleMouseMove);
 
-    // Set a delay after 250ms
-    const delayTimeout = setTimeout(() => {
-      setDelayed(true);
-    }, 250);
-
     return () => {
       window.removeEventListener('scroll', updateScroll);
       window.removeEventListener('mousemove', handleMouseMove);
-      clearTimeout(delayTimeout); // Clear the timeout on cleanup
     };
   }, [controls]);
 
   const gradientStyle = {
-    // backgroundImage: `radial-gradient(at ${mousePosition.x}px ${mousePosition.y}px, rgba(0, 0, 0, 0.4), transparent)`,
+    backgroundImage: `linear-gradient(at ${mousePosition.x}px ${mousePosition.y}px, rgba(0, 0, 0, 0.4), transparent)`,
     backgroundSize: 'cover',
     backgroundAttachment: 'fixed',
   };
 
-  const skewStyle = {
-    transform: `skewX(${(mousePosition.x - window.innerWidth / 2) * 0.01}deg)`,
-    left: `${(mousePosition.x - window.innerWidth / 2) * 0.1}px`,
-    transitionDelay: delayed ? '250ms ease-in-out' : '0ms', // Apply delay when delayed is true
-  };
-
   return (
     <motion.div
-      className="motion-div bg-[#1a1818] fixed top-[-6rem] -z-10 right-[11rem] h-screen w-screen rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#100714]"
-      style={{ ...gradientStyle, ...skewStyle }}
+      className="motion-div bg-[#fbe2e3] fixed top-[-6rem] -z-10 right-[11rem] h-screen w-screen rounded-full blur-[10rem]  dark:bg-[#644343]"
+      style={gradientStyle}
       animate={controls}
     ></motion.div>
   );
