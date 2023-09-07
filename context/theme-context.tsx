7e3b572b431fd49d37f8f1377b72eb06152c9fd1
Remco Stoeten <remcostoeten@hotmail.com>
2023-09-07 20:43:40 +0200

    Fix toggle menu

--- context/theme-context.tsx
+++ context/theme-context.tsx
@@ -32,7 +32,7 @@ export default function ThemeContextProvider({
     };
 
     useEffect(() => {
-        const localTheme = window.localStorage.getItem('theme') as Theme | null;
+        const localTheme = window.localStorage.getItem('dark') as Theme | null;
 
         if (localTheme) {
             setTheme(localTheme);
