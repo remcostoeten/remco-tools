b8fd29efa15a4aac0cc8173ab6ae83901788c5f1
Remco Stoeten <remcostoeten@hotmail.com>
2023-09-08 19:53:40 +0200

    update Typescript

--- components/core/header/Header.tsx
+++ components/core/header/Header.tsx
@@ -13,9 +13,9 @@ export default function Header() {
 
     return (
         <>
-            <header className="z-50 relative">
+            <header className='showAlternativeCursor z-50 relative'>
                 <motion.div
-                    className="header   fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none   bg-white bg-opacity-80 shadow-lg shadow-white/[0.06] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-57rem] sm:rounded-full dark:bg-black "
+                    className='header   fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none   bg-white bg-opacity-80 shadow-lg shadow-white/[0.06] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[58rem] sm:rounded-full dark:bg-black '
                     initial={{ y: -100, x: '-50%', opacity: 0 }}
                     animate={{ y: 0, x: '-50%', opacity: 1 }}></motion.div>
 
@@ -37,8 +37,8 @@ export default function Header() {
 
                                         {link.name === activeSection && (
                                             <motion.span
-                                                className="dark:bg-zinc-950 border rounded-full absolute inset-0 -z-10  dark:active-bg"
-                                                layoutId="activeSection"
+                                                className='dark:bg-zinc-950 border rounded-full absolute inset-0 -z-10  dark:active-bg'
+                                            layoutId='activeSection'
                                                 transition={{
                                                     type: 'spring',
                                                     stiffness: 380,
@@ -47,9 +47,14 @@ export default function Header() {
                                         )}
                                     </Link>
                                 </motion.li>
-                                {index === 3 && (
+                                {index === 4 && (
                                     <>
-                                        <motion.li data-type="video" className="grow" initial={{ y: -100, opacity: 0 }} animate={{ y: 0, opacity: 1 }}>
+                                        <motion.li
+                                            data-type='video'
+                                            className='mouse'
+                                            initial={{ y: -100, opacity: 0 }}
+                                            animate={{ y: 0, opacity: 1 }}
+                                        >
                                             <ToolsDropdown />
                                         </motion.li>
                                         <motion.li initial={{ y: -100, opacity: 0 }} animate={{ y: 0, opacity: 1 }}>
