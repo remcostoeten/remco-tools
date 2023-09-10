986e88a29bf7509260ccd55476fbb110c34dff5e
remcostoeten <remcostoeten@hotmail.com>
2023-09-10 03:45:33 +0200

    rm duplicate

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,21 @@
-import SectionDivider from "@/components/divider";
-import Experience from "@/components/experience";
-import About from "@/components/home/about";
-import Contact from "@/components/home/contact";
-import Footer from "@/components/home/footer";
-import Intro from "@/components/home/intro";
-import Skills from "@/components/home/skills";
+import SectionDivider from '@/components/divider';
+import Experience from '@/components/experience';
+import About from '@/components/home/about';
+import Contact from '@/components/home/contact';
+import Footer from '@/components/home/footer';
+import Intro from '@/components/home/intro';
+import Skills from '@/components/home/skills';
 
 export default function Home() {
-  return (
-    <main className="flex flex-col items-center px-4">
-      <Intro />
-      <SectionDivider />
-      <About />
-      <SectionDivider />
-      <Skills />
-      <Experience />
-      <Footer/>
-      
-      <Contact />
-    </main>
-  );
+    return (
+        <main className="flex flex-col items-center px-4">
+            <Intro />
+            <SectionDivider />
+            <About />
+            <SectionDivider />
+            <Skills />
+            <Experience />
+            <Footer />
+        </main>
+    );
 }
