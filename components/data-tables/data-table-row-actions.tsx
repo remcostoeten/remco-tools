182faa2395dfdaf0ecc243e86280d953c9f0b5f6
remcostoeten <remcostoeten@hotmail.com>
2023-09-10 03:41:59 +0200

    build

--- components/data-tables/data-table-row-actions.tsx
+++ components/data-tables/data-table-row-actions.tsx
@@ -1,84 +1,76 @@
-    'use client';
+'use client';
 
-    import { DotsHorizontalIcon } from '@radix-ui/react-icons';
-    import { Row } from '@tanstack/react-table';
-    import { doc, deleteDoc, collection, getDocs } from 'firebase/firestore';
-    import { Button } from '@/components/ui/button';
-    import {
-        DropdownMenu,
-        DropdownMenuContent,
-        DropdownMenuItem,
-        DropdownMenuRadioGroup,
-        DropdownMenuRadioItem,
-        DropdownMenuSeparator,
-        DropdownMenuShortcut,
-        DropdownMenuSub,
-        DropdownMenuSubContent,
-        DropdownMenuSubTrigger,
-        DropdownMenuTrigger,
-    } from '@/components/ui/dropdown-menu';
+import { DotsHorizontalIcon } from '@radix-ui/react-icons';
+import { Row } from '@tanstack/react-table';
+import { doc, deleteDoc, collection, getDocs } from 'firebase/firestore';
+import { Button } from '@/components/ui/button';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuRadioGroup,
+    DropdownMenuRadioItem,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
+    DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 
-    import { labels } from './data/data';
-    import { db, deleteItem } from '@/utils/firebase';
-    import { toast } from '../ui/use-toast';
+import { labels } from './data/data';
+import { db, deleteItem } from '@/utils/firebase';
+import { toast } from '../ui/use-toast';
 
-    interface DataTableRowActionsProps<TData extends { id: string }> {
-        row: Row<TData>;
-    }
+interface DataTableRowActionsProps<TData extends { id: string }> {
+    row: Row<TData>;
+}
 
-    export function DataTableRowActions<TData>({
-        row,
-    }: DataTableRowActionsProps<TData extends string/>) {
-        const task = row.original;
+export function DataTableRowActions<TData extends { id: string }>({ row }: DataTableRowActionsProps<TData>) {
+    const task = row.original;
 
-        const handleDelete = async () => {
-            try {
-                const taskId = (task as any).id;
-                await deleteItem(taskId);
-                console.log('Task deleted successfully');
-            } catch (error) {
-                console.log(`Failed to delete task: ${error.message}`);
-            }
-        };
+    const handleDelete = async () => {
+        try {
+            const taskId = (task as any).id;
+            await deleteItem(taskId);
+            console.log('Task deleted successfully');
+        } catch (error) {
+            console.log(`Failed to delete task: ${error.message}`);
+        }
+    };
 
-        return (
-            <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                    <Button
-                        variant='ghost'
-                        className='flex h-8 w-8 p-0 data-[state=open]:bg-muted'
-                    >
-                        <DotsHorizontalIcon className='h-4 w-4' />
-                        <span className='sr-only'>Open menu</span>
-                    </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align='end' className='w-[160px]'>
-                    <DropdownMenuItem>Edit</DropdownMenuItem>
-                    <DropdownMenuItem>Make a copy</DropdownMenuItem>
-                    <DropdownMenuItem>Favorite</DropdownMenuItem>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuSub>
-                        <DropdownMenuSubTrigger>Labels</DropdownMenuSubTrigger>
-                        <DropdownMenuSubContent>
-                            {/* @ts-ignore */}
-                            <DropdownMenuRadioGroup value={task.label}>
-                                {labels.map((label) => (
-                                    <DropdownMenuRadioItem
-                                        key={label.value}
-                                        value={label.value}
-                                    >
-                                        {label.label}
-                                    </DropdownMenuRadioItem>
-                                ))}
-                            </DropdownMenuRadioGroup>
-                        </DropdownMenuSubContent>
-                    </DropdownMenuSub>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem onClick={handleDelete}>
-                        Delete
-                        <DropdownMenuShortcut>⌘⌫</DropdownMenuShortcut>
-                    </DropdownMenuItem>
-                </DropdownMenuContent>
-            </DropdownMenu>
-        );
-    }
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="flex h-8 w-8 p-0 data-[state=open]:bg-muted">
+                    <DotsHorizontalIcon className="h-4 w-4" />
+                    <span className="sr-only">Open menu</span>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" className="w-[160px]">
+                <DropdownMenuItem>Edit</DropdownMenuItem>
+                <DropdownMenuItem>Make a copy</DropdownMenuItem>
+                <DropdownMenuItem>Favorite</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuSub>
+                    <DropdownMenuSubTrigger>Labels</DropdownMenuSubTrigger>
+                    <DropdownMenuSubContent>
+                        {/* @ts-ignore */}
+                        <DropdownMenuRadioGroup value={task.label}>
+                            {labels.map((label) => (
+                                <DropdownMenuRadioItem key={label.value} value={label.value}>
+                                    {label.label}
+                                </DropdownMenuRadioItem>
+                            ))}
+                        </DropdownMenuRadioGroup>
+                    </DropdownMenuSubContent>
+                </DropdownMenuSub>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={handleDelete}>
+                    Delete
+                    <DropdownMenuShortcut>⌘⌫</DropdownMenuShortcut>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+}
