93f3c7f886ba5da99354e977bc293e8eb5f801dc
remcostoeten <remcostoeten@hotmail.com>
2023-09-15 13:30:50 +0200

    add inspo

--- app/(nocontainer)/empty/page.tsx
+++ app/(nocontainer)/empty/page.tsx
@@ -1,9 +1,11 @@
+import TileGrid from '@/components/TileGrid';
 import React from 'react';
 
 export default function page() {
     return (
         <>
             <div className="flex flex-col">
+                <TileGrid />
                 <h1>Empty</h1>
             </div>
         </>
--- components/TileGrid.tsx
+++ components/TileGrid.tsx
@@ -1,43 +1,62 @@
 'use client';
 
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
+import * as THREE from 'three';
 
-export default function TileGrid() {
+const TileGrid: React.FC = () => {
     useEffect(() => {
-        const elTextGrid = document.querySelector('.text-grid');
+        // Create a scene
+        const scene = new THREE.Scene();
 
-        function recalculateDistances(mouseCoords) {
-            let containerRect = elTextGrid.getBoundingClientRect();
-            let diagonal = Math.hypot(containerRect.width, containerRect.height);
-            const squares = Array.from(elTextGrid.children);
+        // Create a camera
+        const camera = new THREE.PerspectiveCamera(
+            75, // FOV
+            window.innerWidth / window.innerHeight, // Aspect ratio
+            0.1, // Near clipping plane
+            1000, // Far clipping plane
+        );
 
-            squares.forEach((square: HTMLElement) => {
-                const rect = square.getBoundingClientRect();
-                const distance = Math.hypot(rect.left + rect.width / 2 - mouseCoords.x, rect.top + rect.height / 2 - mouseCoords.y);
-                const normalizedDistance = 1 - distance / diagonal;
-                const colorValue = Math.max(Math.pow(normalizedDistance, 3), 0);
-                square.style.backgroundColor = `rgba(
-          ${255 * colorValue},
-          ${255 * (1 - colorValue)},
-          ${20 + 40 * colorValue},
-          1
-        )`;
-            });
+        // Create a WebGLRenderer
+        const renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(renderer.domElement);
+
+        // Create a black background
+        const backgroundColor = 0x000000;
+        scene.background = new THREE.Color(backgroundColor);
+
+        // Create a grid of white tiles
+        const tileSize = 50;
+        const numRows = Math.ceil(window.innerHeight / tileSize);
+        const numCols = Math.ceil(window.innerWidth / tileSize);
+
+        const tileGeometry = new THREE.BoxGeometry(tileSize, tileSize, 1);
+        const tileMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+
+        for (let row = 0; row < numRows; row++) {
+            for (let col = 0; col < numCols; col++) {
+                const tile = new THREE.Mesh(tileGeometry, tileMaterial);
+                tile.position.set(col * tileSize - window.innerWidth / 2 + tileSize / 2, -row * tileSize + window.innerHeight / 2 - tileSize / 2, 0);
+                scene.add(tile);
+            }
         }
 
-        document.addEventListener('mousemove', (event) => {
-            const { clientX, clientY } = event;
-            recalculateDistances({ x: clientX, y: clientY });
-        });
+        // Set camera position
+        camera.position.z = 100;
+
+        // Animation function
+        const animate = () => {
+            requestAnimationFrame(animate);
+
+            // Rotate the tiles or perform other animations here
+
+            renderer.render(scene, camera);
+        };
+
+        animate();
     }, []);
 
-    return (
-        <div className="text-grid">
-            {Array(11)
-                .fill(null)
-                .map((_, idx) => (
-                    <div key={idx} className="square"></div>
-                ))}
-        </div>
-    );
-}
+    return <div />;
+};
+
+export default TileGrid;
