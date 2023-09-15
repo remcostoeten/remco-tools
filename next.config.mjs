5eaff40c728ea45b5f5a7df66cbd5f7275e78847
Remco Stoeten <remcostoeten@hotmail.com>
2023-09-15 22:16:56 +0200

    Cleanup packages.

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
-    serverActions: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "media.licdn.com",
-      },
-    ],
-  },
+    reactStrictMode: true,
+    experimental: {
+        appDir: true,
+        serverActions: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'media.licdn.com',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
