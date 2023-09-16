3d49017b70755ece4d2a869f660afb076724360e
Remco Stoeten <remcostoeten@hotmail.com>
2023-09-16 23:08:55 +0200

    add stuff

--- app/(nocontainer)/(tests)/empty/page.tsx
+++ app/(nocontainer)/(tests)/empty/page.tsx
@@ -4,9 +4,30 @@ import React, { useEffect } from 'react';
 const Home: React.FC = () => {
     return (
         <>
-            <div className='glowcard ' role='button'>
-                <span className='glow'></span>
-                <div></div>
+            <div className='flex flex-col gap-[125px] justify-between w-screen'>
+                <main className='mt-4 mb-4 border py-20'>
+                    <button aria-label='Say Hello'>
+                        <div>
+                            <span className='inner'>
+                                <div
+                                    className='particles'
+                                    aria-hidden='true'
+                                ></div>
+                                <span className='text'>Say hello</span>
+                                <span
+                                    className='halo'
+                                    aria-hidden='true'
+                                ></span>
+                            </span>
+                        </div>
+                        <span className='shadow' aria-hidden='true'></span>
+                    </button>
+                </main>
+
+                <div className='glowcard ' role='button'>
+                    <span className='glow'></span>
+                    <div></div>
+                </div>
             </div>
         </>
     );
