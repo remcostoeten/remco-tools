42b702dc88044092bfd08e937c7de4fa135a1432
Remco Stoeten <remcostoeten@hotmail.com>
2023-09-17 01:35:53 +0200

    add glowcard

---
+++ components/Readmore.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Readmore() {
+  return (
+    <div><p className="scroll-down"><a href="#complements" className="animate">More</a></p>
+    </div>
+  )
+}
