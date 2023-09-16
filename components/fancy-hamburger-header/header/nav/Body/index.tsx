1d50402a6079006f31de1a3d2eb10f3cf2b9e968
Remco Stoeten <remcostoeten@hotmail.com>
2023-09-16 11:40:23 +0200

    revert header

--- components/fancy-hamburger-header/header/nav/Body/index.tsx
+++ components/fancy-hamburger-header/header/nav/Body/index.tsx
@@ -3,94 +3,63 @@ import { motion } from 'framer-motion';
 import Link from 'next/link';
 import styles from './style.module.scss';
 import { blur, translate } from '../../anim';
-import { useSelectedLink } from '@/context/SelectedLinkContext';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import { useSelectedLink } from '@/context/SelectedLinkContext'; 
 
 interface BodyProps {
-    links: { title: string; href: string }[];
-}
-
-interface SelectedLinkContextValue {
-    selectedLink: { isActive: boolean; index: number };
-    setSelectedLink: React.Dispatch<
-        React.SetStateAction<{ isActive: boolean; index: number }>
-    >;
+  links: { title: string; href: string }[];
 }
 
 function Body({ links }: BodyProps) {
-    const { selectedLink, setSelectedLink } =
-        useSelectedLink() as SelectedLinkContextValue;
-    const router = useRouter();
+  const { selectedLink, setSelectedLink } = useSelectedLink(); // Access the selectedLink state from the context
 
-    useEffect(() => {
-        const handleRouteChange = () => {
-            // Close the menu on route change
-            closeMenuOnClick();
-        };
+  const closeMenuOnClick = () => {
+    setSelectedLink({ isActive: false, index: selectedLink.index });
+  };
 
-        // Listen to the popstate event, which is triggered on history changes
-        window.addEventListener('popstate', handleRouteChange);
+  const getChars = (word: string) => {
+    let chars: JSX.Element[] = [];
+    word.split('').forEach((char, i) => {
+      chars.push(
+        <motion.span
+          custom={[i * 0.02, (word.length - i) * 0.01]}
+          variants={translate}
+          initial="initial"
+          animate="enter"
+          exit="exit"
+          key={char + i}
+        >
+          {char}
+        </motion.span>
+      );
+    });
+    return chars;
+  };
 
-        // Cleanup the event listener when the component is unmounted
-        return () => {
-            window.removeEventListener('popstate', handleRouteChange);
-        };                 
-    }, []);
-
-    const closeMenuOnClick = () => {
-        setSelectedLink({ isActive: false, index: selectedLink.index });
-    };
-
-    const getChars = (word: string) => {
-        let chars: JSX.Element[] = [];
-        word.split('').forEach((char, i) => {
-            chars.push(
-                <motion.span
-                    custom={[i * 0.02, (word.length - i) * 0.01]}
-                    variants={translate}
-                    initial='initial'
-                    animate='enter'
-                    exit='exit'
-                    key={char + i}
-                >
-                    {char}
-                </motion.span>
-            );
-        });
-        return chars;
-    };
-
-    return (
-        <div className={styles.body}>
-            {links.map((link, index) => {
-                const { title, href } = link;
-                return (
-                    <Link key={`l_${index}`} href={href}>
-                        <motion.p
-                            className='showAlternativeCursor text-2xl font-medium'
-                            onMouseOver={() => {
-                                setSelectedLink({ isActive: true, index });
-                            }}
-                            onMouseLeave={() => {
-                                setSelectedLink({ isActive: false, index });
-                            }}
-                            onClick={closeMenuOnClick}
-                            variants={blur}
-                            animate={
-                                selectedLink.isActive &&
-                                selectedLink.index !== index
-                                    ? 'open'
-                                    : 'closed'
-                            }
-                        >
-                            {getChars(title)}
-                        </motion.p>
-                    </Link>
-                );
-            })}
-        </div>
-    );
+  return (
+    <div className={styles.body}>
+      {links.map((link, index) => {
+        const { title, href } = link;
+        return (
+          <Link key={`l_${index}`} href={href}>
+            <motion.p
+              className="showAlternativeCursor text-2xl font-medium"
+              onMouseOver={() => {
+                setSelectedLink({ isActive: true, index });
+              }}
+              onMouseLeave={() => {
+                setSelectedLink({ isActive: false, index });
+              }}
+              onClick={closeMenuOnClick}
+              variants={blur}
+              animate={selectedLink.isActive && selectedLink.index !== index ? 'open' : 'closed'}
+            >
+              {getChars(title)}
+            </motion.p>
+          </Link>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Body;
