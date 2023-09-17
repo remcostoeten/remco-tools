4f5332945384b40d5f227baa30c3059ee8274430
Remco Stoeten <remcostoeten@hotmail.com>
2023-09-17 23:02:53 +0200

    add icons from old repo

---
+++ components/icons/PlusSquareIcon.js
@@ -0,0 +1,66 @@
+export default function PlusSquareIcon() {
+  return (
+    <svg
+      fill="currentColor"
+      height="32"
+      width="32"
+      viewBox="0 0 64 64"
+      style={{ display: "inlineBlock", verticalAlign: "middle" }}
+    >
+      <g>
+        <line
+          strokeWidth="2"
+          strokeMiterlimit="10"
+          x1="32"
+          y1="50"
+          x2="32"
+          y2="14"
+          fill="none"
+          stroke="currentColor"
+        >
+          <line
+            strokeWidth="2"
+            strokeMiterlimit="10"
+            x1="32"
+            y1="50"
+            x2="32"
+            y2="14"
+            fill="none"
+            stroke="currentColor"
+          ></line>
+        </line>
+        <line
+          strokeWidth="2"
+          strokeMiterlimit="10"
+          x1="14"
+          y1="32"
+          x2="50"
+          y2="32"
+          fill="none"
+          stroke="currentColor"
+        >
+          <line
+            strokeWidth="2"
+            strokeMiterlimit="10"
+            x1="14"
+            y1="32"
+            x2="50"
+            y2="32"
+            fill="none"
+            stroke="currentColor"
+          ></line>
+        </line>
+      </g>
+      <rect
+        x="1"
+        y="1"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+        width="62"
+        height="62"
+        fill="none"
+        stroke="currentColor"
+      ></rect>
+    </svg>
+  )
+}
---
+++ components/icons/SmokeIcon.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+function VapeIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="15"
+      height="15"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className="lucide lucide-cigarette"
+      viewBox="0 0 24 24"
+    >
+      <path d="M18 12H2v4h16M22 12v4M7 12v4M18 8c0-2.5-2-2.5-2-5M22 8c0-2.5-2-2.5-2-5"></path>
+    </svg>
+  )
+}
+
+export default VapeIcon
