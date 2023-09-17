9304231f7ea8f83ac2d2d28aea65b0a05eca1442
Remco Stoeten <remcostoeten@hotmail.com>
2023-09-17 23:02:53 +0200

    add icons from old repo

---
+++ components/icons/MoneyIcon.js
@@ -0,0 +1,21 @@
+function MoneyIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className="lucide lucide-dollar-sign"
+      viewBox="0 0 24 24"
+    >
+      <path d="M12 2L12 22"></path>
+      <path d="M17 5H9.5a3.5 3.5 0 000 7h5a3.5 3.5 0 010 7H6"></path>
+    </svg>
+  )
+}
+
+export default MoneyIcon
---
+++ components/icons/test.jsx
@@ -0,0 +1,12 @@
+"use client"
+
+export default function discord({ title }) {
+  return (
+    <>
+      <div clas="discord">
+        <h2 className="someclass">sometext</h2>
+      </div>
+      d
+    </>
+  )
+}
