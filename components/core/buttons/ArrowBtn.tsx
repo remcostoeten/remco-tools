9d6a4b96b3bfc4eb9e618ba04ee8442927c0896c
Remco Stoeten <remcostoeten@hotmail.com>
2023-09-18 22:58:31 +0200

    Add propped btn

--- components/core/buttons/ArrowBtn.tsx
+++ components/core/buttons/ArrowBtn.tsx
@@ -1,79 +1,52 @@
+import React from 'react';
 import Link from 'next/link';
 
-type ArrowBtnProps = {
-    text?: string;
-    anchor?: string;
-    onClick?: () => void;
-    className?: string;
-    showParticles?: boolean;
-    showHoverText?: boolean;
-    hoverText?: string;
+/**
+ * Props for the ArrowButton component.
+ * @typedef {Object} ButtonProps
+ * @property {() => void} [onClick] - Function to be called on button click.
+ * @property {string} [customClassName] - Custom CSS class name for the button.
+ * @property {boolean} [showLeftArrow=true] - Whether to show the left arrow.
+ * @property {boolean} [showRightArrow=true] - Whether to show the right arrow.
+ * @property {string} [hoverText] - Text to display on hover.
+ * @property {React.ReactNode} children - The content inside the button.
+ */
+
+/**
+ * ArrowButton component for displaying an animated button with arrows.
+ * @param {ButtonProps} props - The component props.
+ */
+const ArrowButton = ({
+    onClick,
+    customClassName,
+    showLeftArrow = true,
+    showRightArrow = true,
+    hoverText,
+    children,
+}) => {
+    return (
+        <button className={`btn animbtn ${customClassName}`} onClick={onClick}>
+            <Link href='' className='btn--animated btn__intro animated-arrow'>
+                {showLeftArrow && (
+                    <span className='the-arrow -left'>
+                        <span className='shaft'></span>
+                    </span>
+                )}
+                <span className='main'>
+                    {hoverText && (
+                        <span className='btn__hover-text'>{hoverText}</span>
+                    )}
+                    <span className='absolute text particles'>{children}</span>
+                    {showRightArrow && (
+                        <span className='the-arrow -right'>
+                            <span className='shaft'></span>
+                        </span>
+                    )}
+                </span>
+                <div className='particles'></div>
+            </Link>
+        </button>
+    );
 };
 
-export default function ArrowBtn({
-    text,
-    anchor,
-    onClick,
-    className,
-    showParticles = true,
-    showHoverText = false,
-    hoverText,
-}: ArrowBtnProps) {
-    return (
-        <>
-            <button
-                className={`${className} btn btn--arrow btn--animate btn--transparent`}
-                style={{ width: '200px' }}
-            >
-                <div className='btn__content'>{text}</div>
-                <Link
-                    href={anchor}
-                    onClick={onClick}
-                    className='animated-arrow'
-                >
-                    <span className='the-arrow -left'>
-                        <span className='shaft'></span>
-                    </span>
-                    <span className='main'>
-                        {showHoverText && (
-                            <span className='text particles'>{hoverText}</span>
-                        )}
-                        <span
-                            style={{ transform: ' translateY(-18px)' }}
-                            className='the-arrow               '
-                        >
-                            {' '}
-                            <span className='shaft'></span>
-                        </span>
-                    </span>
-                </Link>
-
-                {showParticles && <div className='particles'></div>}
-            </button>
-            <button className='btn animbtn'>
-                <Link
-                    onClick={() => {
-                        setActiveSection('Contact');
-                        setTimeOfLastClick(Date.now());
-                    }}
-                    className='btn--animated btn__intro animated-arrow'
-                    href='#contact'
-                >
-                    <span className='the-arrow -left'>
-                        <span className='shaft'></span>
-                    </span>
-                    <span className='main'>
-                        <span className='btn__hover-text'>View PDF</span>
-                        <span className='absolute text particles'>Or Download</span>
-                        <span className='the-arrow -right'>
-                            {' '}
-                            <span className='shaft'></span>
-                        </span>
-                    </span>
-                    <div className='particles'></div>
-                </Link>
-            </button>
-        </>
-    );
-}
-// Example usage of ArrowBtn component
+export default ArrowButton;
--- components/home/IntroButtons.tsx
+++ components/home/IntroButtons.tsx
@@ -1,16 +1,21 @@
+import { Arrow } from '@radix-ui/react-popover';
 import React from 'react';
+import ArrowButton from '../core/buttons/ArrowBtn';
 import ArrowBtn from '../core/buttons/ArrowBtn';
 
 export default function IntroButtons() {
+    const handleDownloadPdf = () => {
+        window.open('/resume.pdf', '_blank');
+    };
+
     return (
         <>
-            <ArrowBtn
-                text='Click Me'
-                anchor='#destination'
-                onClick={() => console.log('Button clicked!')}
-                className='custom-class'
-                showParticles={true}
-            />
+            <ArrowButton
+                onClick={handleDownloadPdf} // Set the onClick handler to trigger PDF download
+                hoverText='Download PDF' // Set hoverText to "Download PDF"
+            >
+                Show PDF
+            </ArrowButton>{' '}
         </>
     );
 }
