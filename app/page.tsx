e461135c5b414bf22172f0328cb4ff2756483ba1
Remco Stoeten <remcostoeten@hotmail.com>
2023-10-02 23:33:32 +0200

    update header

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import BlurBackground from '@/components/core/BlurBackground';
 import { MouseHoverEffect } from '@/components/core/MouseHoverEffect';
-import Experience from '@/components/experience';
+import Experience from '@/components/home/experience';
 import About from '@/components/home/about';
 import Intro from '@/components/home/intro';
 import Skills from '@/components/home/skills';
 import { HomeBanners } from '@/components/HomeBanners';
 import SectionSpacer from '@/components/ui/SectionSpacer';
 import AnimatedDiv from '@/utils/AnimatedDiv';
+import Footer from '@/components/landing/Footer';
 export default function Home() {
     return (
         <main className="flex flex-col items-center px-4 sm:px-0">
@@ -21,7 +22,7 @@ export default function Home() {
             <SectionSpacer variant="small" />
             <Experience />
             <SectionSpacer variant="small" />
-            {/* <Footer /> */}
+            <Footer />
         </main>
     );
 }
