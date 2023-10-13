0b64fdfcd3114065571a010af488c09c8b90bcce
Remco Stoeten <remcostoeten@hotmail.com>
2023-10-13 22:06:13 +0200

    modify sprinkle effect some

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Search from '@/components/dashboard/Search';
 import { cn, headerSpacing } from '@/lib/utils';
 import DashboardAsides from '@/components/dashboard/DashboardAsides';
-import { LeftAside } from '@/components/dashboard/LeftAside';
+import LeftAside from '@/components/dashboard/LeftAside';
 
 export default function layout({ children }: { children: React.ReactNode }) {
     return (
